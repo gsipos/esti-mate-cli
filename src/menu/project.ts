95712e46f4aace1f604a0995a31698d294a9cbb5
Gergely Sipos <gergely.sipos@doctusoft.com>
2018-04-15 19:26:02 +0200

    project menu improvements

--- src/menu/project.ts
+++ src/menu/project.ts
@@ -1,5 +1,9 @@
 import inquirer from "inquirer";
-import { Project, wheightedAverageOfTasks, confidenceIntervalOfProject } from "../model";
+import {
+  Project,
+  wheightedAverageOfTasks,
+  confidenceIntervalOfProject
+} from "../model";
 import { showAddTaskPrompt } from "./add-task";
 import { listTasks } from "./list-tasks";
 import Table from "cli-table2";
@@ -7,56 +11,83 @@ import { listTags } from "./list-tags";
 import { saveProjectToFile } from "../files";
 import { chooseProjectMenu } from "./choose-project";
 
+enum ProjectMenuEntry {
+  ADD_TASK = "Add task",
+  SHOW_PROJECT_SUMMARY = "Show project summary",
+  SHOW_TASKS = "Show tasks",
+  SHOW_TAGS = "Show Tags",
+  SAVE = "Save",
+  SELECT_PROJECT = "Select project"
+}
+
 const choices = [
-    'Add task',
-    'Show tasks',
-    'Show tags',
-    'Save',
-    'Select project'
-]
+  ProjectMenuEntry.ADD_TASK,
+  ProjectMenuEntry.SHOW_PROJECT_SUMMARY,
+  ProjectMenuEntry.SHOW_TASKS,
+  ProjectMenuEntry.SHOW_TAGS,
+  ProjectMenuEntry.SAVE,
+  ProjectMenuEntry.SELECT_PROJECT
+];
 
 const ProjectMenu: inquirer.Question = {
-    type: 'list',
-    name: 'selected',
-    message: 'What to do next?',
-    choices
+  type: "list",
+  name: "selected",
+  message: "What to do next?",
+  choices
 };
 
-const separator = '------------------------------------------';
+const separator = "------------------------------------------";
 
 function displayProjectInfo(project: Project) {
-    const interval = confidenceIntervalOfProject(project, project.defaultInterval);
-    const table = new Table();
-    const entries = [
-        { 'Short handle:': project.short },
-        { 'Name:': project.name },
-        { 'Task count:': project.tasks.length },
-        { 'Estimation:': wheightedAverageOfTasks(project.tasks).toFixed(2) },
-        { 'Confidence Interval:': interval.min + ' - ' + interval.max },
-    ];
-    (table as Array<any>).push(...entries);
-    console.log(table.toString());
+  const interval = confidenceIntervalOfProject(
+    project,
+    project.defaultInterval
+  );
+  const table = new Table();
+  const entries = [
+    { "Short handle:": project.short },
+    { "Name:": project.name },
+    { "Task count:": project.tasks.length },
+    { "Estimation:": wheightedAverageOfTasks(project.tasks).toFixed(2) },
+    { "Confidence Interval:": interval.min + " - " + interval.max }
+  ];
+  (table as Array<any>).push(...entries);
+  console.log(table.toString());
+}
+
+function executeOnChoice(
+  entry: ProjectMenuEntry,
+  answers: any,
+  handler: Function
+) {
+  if (entry === answers.selected) {
+    handler();
+  }
 }
 
 export async function showProjectMenu(project: Project) {
+  const answer = await inquirer.prompt(ProjectMenu);
+
+  executeOnChoice(ProjectMenuEntry.SHOW_PROJECT_SUMMARY, answer, () => {
     displayProjectInfo(project);
-    const answer = await inquirer.prompt(ProjectMenu);
-    if (answer.selected === 'Add task') {
-        showAddTaskPrompt(project);
-    }
-    if (answer.selected === 'Show tasks') {
-        listTasks(project.tasks);
-        showProjectMenu(project);
-    }
-    if (answer.selected === 'Show tags') {
-        listTags(project);
-        showProjectMenu(project);
-    }
-    if (answer.selected === 'Save') {
-        saveProjectToFile(project);
-        showProjectMenu(project);
-    }
-    if (answer.selected === 'Select project') {
-        chooseProjectMenu();
-    }
+    showProjectMenu(project);
+  });
+  executeOnChoice(ProjectMenuEntry.ADD_TASK, answer, () =>
+    showAddTaskPrompt(project)
+  );
+  executeOnChoice(ProjectMenuEntry.SHOW_TASKS, answer, () => {
+    listTasks(project.tasks);
+    showProjectMenu(project);
+  });
+  executeOnChoice(ProjectMenuEntry.SHOW_TAGS, answer, () => {
+    listTags(project);
+    showProjectMenu(project);
+  });
+  executeOnChoice(ProjectMenuEntry.SAVE, answer, () => {
+    saveProjectToFile(project);
+    showProjectMenu(project);
+  });
+  executeOnChoice(ProjectMenuEntry.SELECT_PROJECT, answer, () =>
+    chooseProjectMenu()
+  );
 }
