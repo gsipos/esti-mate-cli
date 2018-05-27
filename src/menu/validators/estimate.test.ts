415126285585376ec07ee9dbd2cb38642bbbdf36
Gergely Sipos <gergely.sipos@doctusoft.com>
2018-05-27 14:20:40 +0200

    additional test

--- src/menu/validators/estimate.test.ts
+++ src/menu/validators/estimate.test.ts
@@ -8,6 +8,12 @@ describe('Estimate validators', () => {
         worstCase: 4,
     };
 
+    const floats: Estimate = {
+        bestCase: 2.2,
+        mostLikely: 3.2,
+        worstCase: 4.4,
+    }
+
     it('throws an error for empty string', () => 
         expect(() => parseEstimateString('')).toThrowError());
 
@@ -20,5 +26,15 @@ describe('Estimate validators', () => {
     it('parses space separated integery', () => 
         expect(parseEstimateString('2 3 4')).toEqual(integers));
     
+    it('error if not enough number', () => 
+        expect(() => parseEstimateString('1 2')).toThrowError());
     
+    it('parses float values', () => 
+        expect(parseEstimateString('2.2 3.2 4.4')).toEqual(floats));
+    
+    it('parses comma separated float values', () => 
+        expect(parseEstimateString('2.2, 3.2, 4.4')).toEqual(floats));
+
+    it('throws an error for non numeric characters', () => 
+        expect(() => parseEstimateString('a b c')).toThrowError());
 });
