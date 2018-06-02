f43750ea20616a29749d3a9b0546bf7d14a5e01e
Gergely Sipos <gergely.sipos@doctusoft.com>
2018-06-02 12:34:27 +0200

    create project tests

---
+++ src/menu/create-project.test.ts
@@ -0,0 +1,33 @@
+jest.mock('inquirer');
+jest.mock('../service/files');
+jest.mock('./project');
+
+import inquirer from 'inquirer';
+import { saveProjectToFile } from '../service/files';
+import { CURRENT_PROJECT_VERSION } from '../service/project.parse';
+import { showProjectMenu } from './project';
+import { createProjectMenu } from './create-project';
+
+
+describe('Create Project menu', () => {
+
+    it('creates a project from the answer', async () => {
+        (inquirer.prompt as jest.Mock).mockReturnValueOnce(Promise.resolve({
+            name: 'my awesome project',
+            short: 'MAP',
+            defaultInterval: { value: 95 },
+        }))
+
+        await createProjectMenu();
+
+        expect(saveProjectToFile).toHaveBeenCalledWith(expect.objectContaining({
+            name: 'my awesome project',
+            short: 'MAP',
+            defaultInterval: 95,
+            projectFileVersion: CURRENT_PROJECT_VERSION,
+            tasks: [],
+        }));
+        expect(showProjectMenu).toHaveBeenCalled();
+    });
+
+});
