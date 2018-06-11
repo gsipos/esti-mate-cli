787ae50fc9f572edce13c6811a861919d7409ecc
Gergely Sipos <gergely.sipos@doctusoft.com>
2018-06-11 17:34:11 +0200

    display warning message for outdated versions
    closes #38

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import inquirer from 'inquirer';
 import { chooseProjectMenu } from './menu/choose-project';
+import { appHasNewerVersion, showNewerVersionWarning } from './service/app-version';
 
 console.log("Welcome to esti-mate-cli! Let's draft an estimate!");
 
-chooseProjectMenu();
+appHasNewerVersion()
+    .then(hasNewerVersion => hasNewerVersion ? showNewerVersionWarning() : null)
+    .then(() => chooseProjectMenu());
---
+++ src/service/app-version.ts
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch';
+import { lt as isOutdated } from 'semver';
+import { promisify } from 'util';
+
+const setTimeoutPromise = promisify(setTimeout);
+
+const LATEST_APP_INFO_URL = 'https://registry.npmjs.org/esti-mate-cli/latest';
+
+interface PackageInfo {
+    version: string;
+}
+
+function currentAppVersion() {
+    const packageInfo: PackageInfo = require('../../package.json');
+    return packageInfo.version;
+}
+
+async function getLatestAppVersion() {
+    const response = await fetch(LATEST_APP_INFO_URL);
+    const latestPackageInfo: PackageInfo = await response.json();
+    return latestPackageInfo.version;
+}
+
+export async function appHasNewerVersion() {
+    const current = currentAppVersion();
+    try {
+        const latest = await Promise.race([
+            getLatestAppVersion(),
+            setTimeoutPromise(2000, ''),
+        ]);
+        return isOutdated(current, latest);
+    } catch (err) {
+        return false;
+    }
+}
+
+export function showNewerVersionWarning() {
+    console.warn(' ');
+    console.warn('*******************************************************');
+    console.warn('*');
+    console.warn('* There is a newer version of EstiMate available!');
+    console.warn('* You can update it by running the following command: ');
+    console.warn('* npm i esti-mate-cli -g');
+    console.warn('*');
+    console.warn('*******************************************************');
+    console.warn(' ');
+}
